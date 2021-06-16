4e5e6fecb2383f4b6e7920b66332c526ab227523
Stefan <stefanfgary@gmail.com>
2021-06-16 10:34:27 -0400

    Initial singularity wrapper tested

---
+++ wrap_singularity_oceanparcels.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#==============================================
+# This Singularity container was created from
+# a Docker container whose Conda environment
+# is set up to run as root.  So the container
+# needs to be started with --fakeroot and we
+# DO NOT want the home of root on the host
+# file system, so choose --no-home.
+#
+# Instead, explictly pick whatever PWD the
+# job control tool starts with since that
+# is where the script to be run is most likely
+# to be staged.  The script to be run is
+# given as an input argument to this wrapper.
+#==============================================
+singularity run --no-home --fakeroot --bind ${PWD}:/scratch --pwd /scratch /usr/local/ocean_parcels_latest.sif $*
